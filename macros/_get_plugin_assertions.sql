df2f39f908cbfc455bf8d5a009592433758ec37a
Ryan Julyan <ryan@julyan.biz>
2025-07-16 23:53:10 +0200

    Revert _get_plugin_assertions and add more built_ins

--- macros/_get_plugin_assertions.sql
+++ macros/_get_plugin_assertions.sql
@@ -1,41 +1,34 @@
 {# macros/_get_plugin_assertions.sql #}
 {% macro _get_plugin_assertions(assertions_dict) %}
-    {%- set plugin_rules = {} %}
-    {%- set built_ins    = ['__unique__', '__not_null__'] %}
-    {%- set adapter      = target.adapter or '' %}
+    {%- set plugin_rules = {} -%}
+    {%- set built_ins = ['__unique__', '__not_null__' ,'__is_positive__' ,'__is_negative__' ,'__is_greater_than__' ,'__is_greater_or_equal_to__' ,'__is_less_than__' ,'__is_less_or_equal_to__' ,'__is_in__' ,'__is_not_in__' ,'__is_between__' ,'__has_regex_pattern__' ,'__has_max__' ,'__has_min__' ,'__has_sum__' ,'__has_count__' ,'__has_mean__' ,'__has_max_by__' ,'__has_min_by__' ,'__has_sum_by__' ,'__has_count_by__' ,'__has_mean_by__' ,'__is_today__' ,'__is_yesterday__' ,'__is_on_weekday__' ,'__is_on_weekend__' ,'__is_on_monday__' ,'__is_on_tuesday__' ,'__is_on_wednesday__' ,'__is_on_thursday__' ,'__is_on_friday__' ,'__is_on_saturday__' ,'__is_on_sunday__'] %}
 
-    {%- for helper_key, cols in assertions_dict.items() | list %}
-        {%- if helper_key.startswith('__')
-              and helper_key.endswith('__')
-              and helper_key not in built_ins %}
+    {# Copy keys first so we can mutate the dict safely #}
+    {%- set helper_keys = assertions_dict.keys() | list -%}
 
-            {# "__is_positive__" -> "is_positive" -> "dbt_assertions__is_positive" #}
-            {%- set rule          = helper_key[2:-2] %}
-            {%- set generic_name  = 'dbt_assertions__' ~ rule %}
-            {%- set adapter_name  = adapter ~ '__' ~ generic_name if adapter else None %}
+    {%- for helper_key in helper_keys %}
+        {%- if helper_key.startswith('__') and helper_key.endswith('__')
+             and helper_key not in built_ins %}
 
-            {# Find first macro whose key ends with adapter_name or generic_name #}
-            {%- set plugin_macro = None %}
-            {%- for k, v in context.items() if v is mapping or v is callable %}
-                {%- if plugin_macro is none %}
-                    {%- if adapter_name and k.endswith(adapter_name) %}
-                        {%- set plugin_macro = v %}
-                    {%- elif k.endswith(generic_name) %}
-                        {%- set plugin_macro = v %}
-                    {%- endif %}
-                {%- endif %}
-            {%- endfor %}
+            {# "__positive__" → "positive" → "dbt_assertions__positive" #}
+            {%- set rule_name   = helper_key[2:-2] %}
+            {%- set macro_name  = 'dbt_assertions__' ~ rule_name %}
+            {%- set plugin_macro = context.get(macro_name) %}
 
             {%- if plugin_macro is none %}
                 {{ exceptions.raise_compiler_error(
                     'Helper key ' ~ helper_key ~
-                    ' could not find a macro ending with "' ~
-                    (adapter_name or generic_name) ~ '"'
+                    ' needs a macro called ' ~ macro_name ~
+                    ' (not found in project or packages).'
                 ) }}
+            {%- else %}
+                {%- set cols = assertions_dict[helper_key] %}
+                {%- set new_rules = plugin_macro(cols) %}
+                {%- do plugin_rules.update(new_rules) %}
             {%- endif %}
 
+            {# Remove the helper key so _assertions_expression never sees it #}
             {%- do assertions_dict.pop(helper_key) %}
-            {%- do plugin_rules.update(plugin_macro(cols)) %}
         {%- endif %}
     {%- endfor %}
 
